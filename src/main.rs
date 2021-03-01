5ae9b046664d2342dac82f4f4cbc374bb4fcafdd
Ryan Clanton <ryanc@Insightec.com>
2021-03-01 14:31:08 -0500

    say which ones are in folder with path and say which ones are not

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,12 @@ fn main() {
             .filter_map(Result::ok)
             .filter(|e| !e.file_type().is_dir()) {
         let f_name = String::from(entry.file_name().to_string_lossy());
+		let path = String::from(entry.path().to_string_lossy());
         let counter = raw_filenames.entry(f_name.clone()).or_insert(0);
         *counter += 1;
         if (*counter == 1) && (f_name.ends_with(".dll") || f_name.ends_with(".pyd") || f_name.ends_with(".exe")){
-			filt_filenames.entry(f_name.clone()).or_insert(0);
-			let file = fs::read(&entry.path().to_owned()).unwrap();
+			filt_filenames.entry(f_name.clone()).or_insert(path);
+			let file = fs::read(&entry.path()).unwrap();
 			let win_pe = match Object::parse(&file).unwrap() {
 				Object::PE(pe) => pe,
 				_ => {
@@ -28,11 +29,12 @@ fn main() {
 				None => vec![],
 				Some(v) => v.import_data,
 			};
+
 			for cur_import in &import_data {
 				let counter = raw_filenames.entry(cur_import.name.to_owned().clone()).or_insert(0);
 				*counter += 1;
 				if *counter == 1 {
-					filt_filenames.entry(cur_import.name.to_owned().clone()).or_insert(0);
+					filt_filenames.entry(cur_import.name.to_owned().clone()).or_insert("Not in folder".to_string());
 				}
 				}
 		}
